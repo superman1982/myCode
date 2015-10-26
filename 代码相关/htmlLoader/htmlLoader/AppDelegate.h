a7b733de7daf57e6547fa60a440476a488d91a93
lin <530139052@qq.com>
2015-10-26 09:48:16 +0800

    更改XOCDE7.1的启动时没设置RootViewController的crash

---
+++ 代码相关/htmlLoader/htmlLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  htmlLoader
+//
+//  Created by lin on 15/10/23.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 代码相关/htmlLoader/htmlLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  htmlLoader
+//
+//  Created by lin on 15/10/23.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
